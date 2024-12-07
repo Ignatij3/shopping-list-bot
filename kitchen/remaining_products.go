2a82f89507c384305be72de764d28c4985806b93
Ignatijs <ignatijs.krasovskis@gmail.com>
2024-12-07 16:13:36 +0100

    Usability additions

--- kitchen/remaining_products.go
+++ kitchen/remaining_products.go
@@ -2,13 +2,13 @@ package kitchen
 
 import (
 	"main/mylogger"
-	recipes "main/recipes"
+	"main/recipes"
 )
 
 // map in which a key is a name of product and number is amount in grams of how much products are left.
 type RemainingProducts map[string]int
 
-// FindMatchingRecipe will return all recipes that it is possible to produce using "rmProd".
+// FindMatchingRecipe will return all recipes that it is possible to produce using remaining products.
 // Dist - is amount of products that are allowed to be in quantities not high enough to cook a recipe.
 // Second returned value is true if a recipe is found.
 // Beware that if one recipe is made, it is not guaranteed that other recipes still can be made
@@ -35,21 +35,41 @@ func (rp RemainingProducts) FindMatchingRecipes(rec recipes.Recipes, dist int) (
 	return result, false
 }
 
+// SetProductWeight sets specified weight to the product.
+// If the product does not exist, false is returned.
+// It is preferred to use this function to prevent nonsense ingredients from coming up.
+func (p *RemainingProducts) SetProductWeight(name string, weight int) (successful bool) {
+	if _, ok := (*p)[name]; ok {
+		(*p)[name] = weight
+		successful = true
+	}
+	return
+}
+
 // AddToProduct adds weight to product.
-// If the product does not exist, it is added.
-func (p *RemainingProducts) AddToProduct(name string, weight int) {
-	(*p)[name] += weight
+// If the product does not exist, false is returned.
+// It is preferred to use this function to prevent nonsense ingredients from coming up.
+func (p *RemainingProducts) AddToProduct(name string, weight int) (successful bool) {
+	if _, ok := (*p)[name]; ok {
+		(*p)[name] += weight
+		successful = true
+	}
+	return
 }
 
 // ReduceProduct reduces weight of product.
-func (p *RemainingProducts) ReduceProduct(name string, weight int) {
+// If the product does not exist, false is returned.
+// If the product weight is smaller than that of the argument, the product is deleted.
+func (p *RemainingProducts) ReduceProduct(name string, weight int) (successful bool) {
 	if _, ok := (*p)[name]; ok {
 		if (*p)[name] < weight {
 			delete((*p), name)
 		} else {
 			(*p)[name] -= weight
 		}
+		successful = true
 	}
+	return
 }
 
 // DeleteProduct deleted the product's weight.
@@ -61,7 +81,9 @@ func (p *RemainingProducts) DeleteProduct(name string) {
 func (p *RemainingProducts) CookRecipe(rcp recipes.Recipe) {
 	mylog.Printf(mylogger.INFO+"CookRecipe is called with arguments: %v\n", rcp)
 	for name, prod := range rcp.GetIngredients() {
-		(*p).ReduceProduct(name, prod)
+		if !(*p).ReduceProduct(name, prod) {
+			mylog.Printf(mylogger.WARN+"Product %s is not found when cooking %v\n", name, rcp)
+		}
 	}
 }
 
@@ -69,6 +91,8 @@ func (p *RemainingProducts) CookRecipe(rcp recipes.Recipe) {
 func (p *RemainingProducts) UnCookRecipe(rcp recipes.Recipe) {
 	mylog.Printf(mylogger.INFO+"UnCookRecipe is called with arguments: %v\n", rcp)
 	for name, prod := range rcp.GetIngredients() {
-		(*p).AddToProduct(name, prod)
+		if !(*p).AddToProduct(name, prod) {
+			mylog.Printf(mylogger.WARN+"Product %s is not found when UNcooking %v\n", name, rcp)
+		}
 	}
 }
